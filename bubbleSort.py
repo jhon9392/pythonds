f5b1f63663bce2d525e4613982052fff90fb3e20
jhon9392 <33473471+jhon9392@users.noreply.github.com>
2022-05-07 11:21:50 +0530

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -10,3 +10,20 @@ def bubbleSort(alist):
     print(alist)
     return alist
 bubbleSort([10,9,8,7,6,5,4,3,2,1])
+
+def bubbleSort1(alist):
+    exchanges = True
+    l = len(alist)-1
+    while l > 0 and exchanges:
+        exchanges = False
+        for i in range(l):
+            if alist[i] > alist[i+1]:
+                exchanges = True
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+        l = l -1
+    print(alist)
+    return alist
+bubbleSort([10,9,8,7,6,5,4,3,2,1])
+        
